e570d810e3ee91af7db3ef466b0b1ace7aa392ad
MikeMac <micheledong2803@gmail.com>
2024-11-05 18:42:59 +0100

    12 refetch data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,28 +1,32 @@
-'use server';
-import { z } from 'zod';
-import { sql } from '@vercel/postgres';
- 
+"use server";
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  status: z.enum(["pending", "paid"]),
   date: z.string(),
 });
- 
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const {customerId, amount, status} = CreateInvoice.parse({
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
   const amountInCents = amount * 100; // for greater precision
-  const date = new Date().toISOString().split('T')[0];
+  const date = new Date().toISOString().split("T")[0];
 
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  // Since you're updating the data displayed in the invoices route, you want to clear this
+  // cache and trigger a new request to the server. You can do this with the revalidatePath function from Next.js
+  revalidatePath("/dashboard/invoices");
 }
