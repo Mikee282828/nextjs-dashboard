afadc58486581528a617bfb6d7d6a7be96e75f47
MikeDesktop <micheledong2803@gmail.com>
2025-02-26 15:46:17 +0100

    11 debouncing library

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,26 +1,26 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { log } from 'console';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { log } from "console";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
-
   const searchParams = useSearchParams(); // Allows you to access the parameters of the current URL
-  const pathname = usePathname();         // Allows you to access the path
-  const { replace } = useRouter();        //  Enables navigation between routes within client components programmatically
-  
-  function handleSearch(term: string){
-    const params = new URLSearchParams(searchParams);   // URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters.
-    
-    if(term){
-      params.set('query',term);
-    }else{
-      params.delete('query');
-    }
+  const pathname = usePathname(); // Allows you to access the path
+  const { replace } = useRouter(); //  Enables navigation between routes within client components programmatically
 
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams); // URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters.
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -30,10 +30,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e)=>{
+        onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
