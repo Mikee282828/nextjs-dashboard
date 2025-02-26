6a74884fc771d31f26efe5d7e4164522423703e8
MikeDesktop <micheledong2803@gmail.com>
2025-02-26 21:06:44 +0100

    12 read id from params

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,7 +7,6 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { CreateInvoice } from './buttons';
 import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
