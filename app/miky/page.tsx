f0b299fc0687097318167a1d0f5ff874d0ba70ff
MikeDesktop <micheledong2803@gmail.com>
2025-03-03 13:12:02 +0100

    apos fix

--- app/miky/page.tsx
+++ app/miky/page.tsx
@@ -2,8 +2,8 @@
 export default function Page(){
     return(
         <div>
-            <h1>Welcome to Miky's Page</h1>
+            <h1>Welcome to Miky&apos;s Page</h1>
             <p>This is a sample page component in your Next.js dashboard.</p>
         </div>
-    )
+    );
 }
